15186e06c5df1d93ad32fa4769f9e38c86a81c70
janklod7 <omerclos@mail.tau.ac.il>
2022-08-13 14:41:27 +0300

    extended to use features

--- datasets/English_data.py
+++ datasets/English_data.py
@@ -1,24 +1,23 @@
 import json
-import math
-from typing import Tuple
 
 import torch
 import yaml
 import os
-from torch import nn, Tensor
 import torch.nn.functional as F
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
-from torch.utils.data import dataset
 import requests
+
+from datasets.english_features import EngFeatures
 from datasets.language_loader import Language
 
 
 class EngLang(Language):
-    def __init__(self, root, params, data=None, use_plurals=False, savenew=False):
+    def __init__(self, root, params, data=None, use_plurals=False, use_features=False, savenew=False):
         self.root = root
         with open(root+params) as f:
             self.config = yaml.load(f, Loader=yaml.BaseLoader)['language']
         mx = int(self.config['maximum_word_length'])
+        if use_features:
+            mx = str(mx)+'f'
         if data and os.path.isfile(f"{root+data}/data_{mx}.json"):
             data = root+data
             with open(f"{data}/data_{mx}.json") as f:
@@ -30,7 +29,10 @@ class EngLang(Language):
             with open(f"{data}/plurals_{mx}.json") as f:
                 self.plurals = json.load(f)
         else:
-            self.parse_new(savenew)
+            if use_features:
+                self.parse_new_features(savenew)
+            else:
+                self.parse_new(savenew)
         if use_plurals:
             self.data.update(self.plurals)
 
@@ -41,8 +43,8 @@ class EngLang(Language):
         self.data = {}
         self.index_to_phon = {0: ""}
         self.phonemes = {"": 0}
-        self.phonemes["PLUR"] = 1
-        self.index_to_phon[1] = "PLUR"
+        # self.phonemes["PLUR"] = 1
+        # self.index_to_phon[1] = "PLUR"
         toolong = 0
         maxlen = 0
         ip = 2
@@ -63,12 +65,13 @@ class EngLang(Language):
                     self.index_to_phon[ip] = p
                     ip += 1
         pa = self.root + "datasets/Languages/English"
-        with open(f"{pa}/phonemes_{self.config['maximum_word_length']}.json", 'w') as fp:
-            json.dump(self.phonemes, fp)
-        with open(f"{pa}/data_{self.config['maximum_word_length']}.json", 'w') as fp:
-            json.dump(self.data, fp)
-        with open(f"{pa}/index_to_phon_{self.config['maximum_word_length']}.json", 'w') as fp:
-            json.dump(self.index_to_phon, fp)
+        if save:
+            with open(f"{pa}/phonemes_{self.config['maximum_word_length']}.json", 'w') as fp:
+                json.dump(self.phonemes, fp)
+            with open(f"{pa}/data_{self.config['maximum_word_length']}.json", 'w') as fp:
+                json.dump(self.data, fp)
+            with open(f"{pa}/index_to_phon_{self.config['maximum_word_length']}.json", 'w') as fp:
+                json.dump(self.index_to_phon, fp)
 
         self.plurals = {}
         for w, p in self.data.items():
@@ -82,9 +85,9 @@ class EngLang(Language):
 
                 elif w[-3:] == "IES" and w[:-3] + "Y" in self.data:
                     self.plurals[w[:-3] + "_PLUR"] = (self.data[w[:-3] + "Y"][0][:-1] + ["PLUR", "END"], p)
-
-        with open(f"{pa}/plurals_{self.config['maximum_word_length']}.json", 'w') as fp:
-            json.dump(self.plurals, fp)
+        if save:
+            with open(f"{pa}/plurals_{self.config['maximum_word_length']}.json", 'w') as fp:
+                json.dump(self.plurals, fp)
 
         # for w in self.plurals:
         #     self.data[w] = self.plurals[w]
@@ -92,6 +95,74 @@ class EngLang(Language):
         print(f"Got {len(self.data) - len(self.plurals)} words")
         print(f"Got {len(self.phonemes)} phonemes")
         print(f"Got {len(self.plurals)} plurals")
+        print(f"Word maximum length is {maxlen}")
+        print(f"{toolong} Words were too long")
+
+    def parse_new_features(self, save=False):
+        response = requests.get("http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b").text.split("\n")
+        self.data = {}
+        self.features = EngFeatures()
+        self.phonemes = {}
+        self.index_to_phon = {}
+        for i, p in enumerate(self.features.phon_lst):
+            self.phonemes[p] = i
+            self.index_to_phon[i] = p
+        toolong = 0
+        maxlen = 0
+        for d in response:
+            if len(d) == 0 or d[0] == ";":
+                continue
+            d = d.split(" ")
+            phonms = ["BEG"] + d[2:] + ["END"]
+            new_phonms = []
+            for p in phonms:
+                if p != 'AH0':
+                    p = p.replace("0", "")
+                    p = p.replace("1", "")
+                    p = p.replace("2", "")
+                if p in self.features.replace:
+                    new_phonms.extend(self.features.replace[p])
+                else:
+                    new_phonms.append(p)
+            phonms = new_phonms
+            if len(phonms) > int(self.config['maximum_word_length']):
+                toolong += 1
+                continue
+            self.data[d[0]] = (phonms, phonms)
+            if len(phonms) > maxlen:
+                maxlen = len(phonms)
+        pa = self.root + "datasets/Languages/English"
+        if save:
+            with open(f"{pa}/phonemes_{self.config['maximum_word_length']}f.json", 'w') as fp:
+                json.dump(self.phonemes, fp)
+            with open(f"{pa}/data_{self.config['maximum_word_length']}f.json", 'w') as fp:
+                json.dump(self.data, fp)
+            with open(f"{pa}/index_to_phon_{self.config['maximum_word_length']}f.json", 'w') as fp:
+                json.dump(self.index_to_phon, fp)
+
+        # self.plurals = {}
+        # for w, p in self.data.items():
+        #     if len(w) >= 4 and w[-1] == "S":
+        #         if w[:-1] in self.data:
+        #             # self.plurals.append((w[:-1], w))
+        #             self.plurals[w[:-1] + "_PLUR"] = (self.data[w[:-1]][0][:-1] + ["PLUR", "END"], p)
+        #
+        #         elif w[:-2] in self.data and w[-2] == "E":
+        #             self.plurals[w[:-2] + "_PLUR"] = (self.data[w[:-2]][0][:-1] + ["PLUR", "END"], p)
+        #
+        #         elif w[-3:] == "IES" and w[:-3] + "Y" in self.data:
+        #             self.plurals[w[:-3] + "_PLUR"] = (self.data[w[:-3] + "Y"][0][:-1] + ["PLUR", "END"], p)
+        # if save:
+        #     with open(f"{pa}/plurals_{self.config['maximum_word_length']}.json", 'w') as fp:
+        #         json.dump(self.plurals, fp)
+
+        # for w in self.plurals:
+        #     self.data[w] = self.plurals[w]
+
+        # print(f"Got {len(self.data) - len(self.plurals)} words")
+        print(f"Got {len(self.data)} words")
+        print(f"Got {len(self.phonemes)} phonemes")
+        # print(f"Got {len(self.plurals)} plurals")
         print(f"Word maximum length is {maxlen}")
         print(f"{toolong} Words were too long")
 
@@ -113,6 +184,6 @@ class EngLang(Language):
 
 
 if __name__ == "__main__":
-    eng = EngLang("english_config.yaml", data="Languages/English")
+    eng = EngLang("C:/repos/TransPhono/","datasets/english_config.yaml", data="Languages/English", use_features=True, savenew=True)
     print(list(eng.data.values())[:10])
     print(eng.phonemes)
